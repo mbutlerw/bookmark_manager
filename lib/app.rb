3bbb0cf5d480a6df06b9b73c83999b8602c1910b
Elena <elena.vilimaite@gmail.com>
2016-06-14 13:31:58 +0100

    tags'

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,7 @@
-require 'sinatra/base'
-require_relative 'models/link'
 ENV["RACK_ENV"] ||= "development"
+
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 	get '/links' do
@@ -13,8 +14,19 @@ class BookmarkManager < Sinatra::Base
 	end
 
 	post '/links' do
-		Link.create(url: params[:url], title: params[:title])
+		link = Link.create(url: params[:url], title: params[:title])
+		params[:tags].split.each do |tag|
+			link.tags << Tag.create(name: tag)
+		end
+		link.save
+
 		redirect '/links'
+	end
+
+	get '/tags/:name' do
+		tag = Tag.first(name: params[:name])
+		@links = tag ? tag.links : []
+		erb :'links/index'
 	end
 
 	# start the server if ruby file executed directly
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -21,7 +21,7 @@ feature 'Adding links' do
   scenario 'to add new link to list' do
     visit '/links/new'
 
-    fill_in :url, with: 'http://www.google.com' 
+    fill_in :url, with: 'http://www.google.com'
     fill_in :title, with: 'Google Search'
     click_button 'Add Bookmark'
 
@@ -30,3 +30,53 @@ feature 'Adding links' do
     end
   end
 end
+
+feature 'Adding links with a tag' do
+
+  scenario 'to add new link to list' do
+    visit '/links/new'
+
+    fill_in :url, with: 'http://www.buzzfeed.com'
+    fill_in :title, with: 'Buzzfeed'
+    fill_in :tags, with: 'news'
+
+    click_button 'Add Bookmark'
+
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('news')
+  end
+end
+
+feature 'Viewing links' do
+
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
+  scenario 'filter links by tag' do
+    visit '/tags/bubbles'
+
+    expect(page.status_code).to eq(200)
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('Code.org')
+      expect(page).to have_content('This is Zombocom')
+      expect(page).to have_content('Bubble Bobble')
+    end
+  end
+
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags',  with: 'education ruby'
+    click_button 'Add Bookmark'
+    link = Link.first
+
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
+end
+
