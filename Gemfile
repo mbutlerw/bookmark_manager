d93c878cd85f3b78cf88b4f35cc317c7568e913f
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-14 14:59:43 +0100

    added user management

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
+gem 'bcrypt'
 
 group :test do
   gem 'rspec'
--- lib/app.rb
+++ lib/app.rb
@@ -4,6 +4,10 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+
+	enable :sessions
+	set :session_secret, 'super secret'
+
 	get '/links' do
 		@links = Link.all
 		erb :'links/index'
@@ -29,6 +33,23 @@ class BookmarkManager < Sinatra::Base
 		erb :'links/index'
 	end
 
+	get '/users/new' do
+		erb :'users/new'
+	end
+
+	post '/users' do
+	  user = User.create(email: params[:email],
+	              password: params[:password],
+								password_confirmation: params[:password_confirmation])
+		session[:user_id] = user.id
+	  redirect to('/links')
+	end
+
+	helpers do
+		def current_user
+			@current_user ||= User.get(session[:user_id])
+		end
+	end
 	# start the server if ruby file executed directly
 	run! if app_file == $0
 end
