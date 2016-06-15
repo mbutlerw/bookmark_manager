99819e5241d8a827a895ac8d3008a45000e91ac9
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-15 15:44:03 +0100

    added password confirmation

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,9 +1,12 @@
 feature 'Sign_up_users' do
-  
+
   scenario 'sign up and new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
+
+  scenario 'incorrect password confirmation' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
 end
-  
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,8 +1,9 @@
-def sign_up
-
-  visit('/users/new')
-  fill_in :email, with: 'alice@example.com'
-  fill_in :password, with: 'abc123'
-  click_button 'Sign up'
-
+def sign_up(email: 'alice@example.com',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
 end
