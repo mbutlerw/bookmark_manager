22b2fe7637f8a248de165d27c6775259f9cb0210
riyapabari <riya.pabari@gmail.com>
2016-06-15 17:26:22 +0100

    Chapter 18: added layout view

--- app/app.rb
+++ app/app.rb
@@ -46,5 +46,11 @@ class BookmarkManager < Sinatra::Base
     erb(:'links/index')
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   run! if app_file == $0
 end
