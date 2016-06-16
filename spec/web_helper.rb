7f671c1a3438a5302685dffe0eb0d005b6e8e49e
Luke <lukeaveil@yahoo.com>
2016-06-16 15:42:14 +0100

    add password confirmation

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,10 @@
-def sign_up
+def sign_up(email: 'luke@example.com',
+			password: '1234',
+			password_confirmation: '1234')
   visit '/users/new'
   #expect(page.status_code).to eq(200)
-  fill_in :email, with: 'luke@example.com'
-  fill_in :password, with: 'turkey'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
