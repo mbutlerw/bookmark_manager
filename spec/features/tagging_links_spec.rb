46ba9096332432a0b42fc135286171f56f75d32b
Luke <lukeaveil@yahoo.com>
2016-06-16 11:10:15 +0100

    multiple tags

--- spec/features/tagging_links_spec.rb
+++ spec/features/tagging_links_spec.rb
@@ -10,4 +10,14 @@ feature 'Adding tags to links' do
     link = Link.first
     expect(link.tags.map(&:name)).to include('art')
   end
+
+  scenario 'I can add multiple tags to a link' do
+    visit '/links/new'
+    fill_in 'url', with: 'butdoesitfloat.com'
+    fill_in 'title', with: 'but does it float'
+    fill_in 'tags', with: 'art photography'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('art', 'photography')
+  end
 end
