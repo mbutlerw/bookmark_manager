fe33d0e684427eb27def3975dc8b0317c066731d
Luke <lukeaveil@yahoo.com>
2016-06-16 15:05:48 +0100

    add user accounts

--- app/app.rb
+++ app/app.rb
@@ -2,9 +2,19 @@ ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require_relative './lib/link'
 require_relative './lib/tag'
+require_relative './lib/user'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   get '/' do
   	"Hello!"
   end
@@ -31,6 +41,17 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect '/links'
   end
 
   run! if app_file == $0
